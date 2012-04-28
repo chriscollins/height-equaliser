e9f46698a07b320f5bdd3dd4770361b9e6d95855
Chris Collins <chrisjamescollins@gmail.com>
2012-04-28 17:03:59 +0100

    Added ability to cope with measureSelectors that match multiple elements.  Added some qunit tests.

--- heightEqualiser.js
+++ heightEqualiser.js
@@ -8,7 +8,7 @@
  * Documentation and usage available at https://github.com/chriscollins/height-equaliser
  */
 
-/*global jQuery*/
+/*global jQuery, console*/
 
 // Closure.
 (function ($) {
@@ -43,14 +43,36 @@
                     measureContainer = null;
 
                 $.each(options.targets, function (index, target) {
-                    var measureContainer = $this.find(target.measureSelector);
-                    if (measureContainer) {
-                        measureHeight = getIntegerValueFromCssProperty(measureContainer.css('height'));
-                        maxHeight = Math.max(measureHeight, maxHeight);
+                    measureContainer = $this.find(target.measureSelector);
+                    if (measureContainer.length > 0) {
+                        measureContainer.each(function () {
+                            measureHeight = getIntegerValueFromCssProperty($(this).css('height'));
+                            maxHeight = Math.max(measureHeight, maxHeight);
+                        });
+                    } else {
+                        //console.log('Measure container "' + target.measureSelector + '" was not found.');
                     }
                 });
+                //console.log('Maximum height is "' + maxHeight + 'px.');
 
                 return maxHeight;
+            }
+
+            /**
+             * Get the name of the CSS property to adjust for the target passed in.  Falls back to options.defaultAdjustProperty if not specified.
+             *
+             * @param  {Object} target The target to get the adjustProperty for.
+             *
+             * @return {string} The name of the CSS property to adjust for the target passed in.
+             */
+            function getAdjustProperty(target) {
+                var adjustProperty = options.defaultAdjustProperty;
+
+                if (target.adjustProperty !== undefined) {
+                    adjustProperty = target.adjustProperty;
+                }
+
+                return adjustProperty;
             }
 
             /**
@@ -63,19 +85,25 @@
                     difference = 0,
                     adjustContainer = null,
                     currentAdjustPropertyValue = 0,
-                    newValue = 0;
+                    newValue = 0,
+                    adjustProperty = '';
 
                 $.each(options.targets, function (index, target) {
                     measureContainer = $this.find(target.measureSelector);
-                    measureHeight = getIntegerValueFromCssProperty(measureContainer.css('height'));
-                    difference = maxHeight - measureHeight;
+                    if (measureContainer.length > 0) {
+                        measureContainer.each(function () {
+                            measureHeight = getIntegerValueFromCssProperty($(this).css('height'));
+                            difference = maxHeight - measureHeight;
 
-                    if (difference > 0) {
-                        adjustContainer = $this.find(target.adjustSelector);
-                        currentAdjustPropertyValue = getIntegerValueFromCssProperty(adjustContainer.css(target.adjustProperty));
-                        newValue = (currentAdjustPropertyValue + difference) + 'px';
-                        adjustContainer.css(target.adjustProperty, newValue);
-                        //console.log('Added "' + difference + 'px" of "' + target.adjustProperty + '" to "' + target.adjustSelector + '" (new value "' + newValue + '").');
+                            if (difference > 0) {
+                                adjustContainer = $this.find(target.adjustSelector);
+                                adjustProperty = getAdjustProperty(target);
+                                currentAdjustPropertyValue = getIntegerValueFromCssProperty(adjustContainer.css(adjustProperty));
+                                newValue = (currentAdjustPropertyValue + difference) + 'px';
+                                adjustContainer.css(adjustProperty, newValue);
+                                //console.log('Added "' + difference + 'px" of "' + adjustProperty + '" to "' + target.adjustSelector + '" (new value "' + newValue + '").');
+                            }
+                        });
                     }
                 });
             }
@@ -86,6 +114,7 @@
 
     // Plugin defaults.
     $.fn.heightEqualiser.defaults = {
+        "defaultAdjustProperty": 'height' // The CSS property to adjust by default.
     };
 
 // End closure.
---
+++ test/tests.js
@@ -0,0 +1,195 @@
+/**
+ * QUnit tests for heightEqualiser.js
+ */
+
+/*global test, module, $*/
+
+module('Main');
+test('Padding is applied to adjustSelector to equalise heights', function () {
+    var column1 = $('#main .column1'),
+        column2 = $('#main .column2'),
+        column1Adjust = column1.find('ul.items .last'),
+        column2Adjust = column2.find('.title'),
+        initialPaddingAdjustColumn1 = column1Adjust.css('paddingBottom'),
+        initialPaddingAdjustColumn2 = column2Adjust.css('paddingBottom'),
+        wasColumn1Adjusted = false,
+        wasColumn2Adjusted = false;
+
+    ok(column1.css('height') !== column2.css('height')); // Ensure heights are initially different.
+
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#main .column1',
+               "adjustSelector": '#main .column1 ul.items .last',
+               "adjustProperty": 'paddingBottom'
+           },
+           {
+               "measureSelector": '#main .column2',
+               "adjustSelector": '#main .column2 .title',
+               "adjustProperty": 'paddingBottom'
+           }
+        ]
+    });
+
+    ok(column1.css('height') === column2.css('height')); // Heights should now match.
+
+    wasColumn1Adjusted = column1Adjust.css('paddingBottom') !== initialPaddingAdjustColumn1;
+    wasColumn2Adjusted = column2Adjust.css('paddingBottom') !== initialPaddingAdjustColumn2;
+
+    ok((wasColumn1Adjusted && !wasColumn2Adjusted) || (!wasColumn1Adjusted && wasColumn2Adjusted)); // Padding was either applied to column1's adjustSelector, or column2's adjustSelector: Not both.
+});
+
+test('Initial values of padding are respected', function () {
+    var column1 = $('#with-initial-padding .column1'),
+        column2 = $('#with-initial-padding .column2'),
+        column1Adjust = column1.find('ul.items .last'),
+        column2Adjust = column2.find('.title'),
+        initialPaddingAdjustColumn1 = column1Adjust.css('paddingBottom'),
+        initialPaddingAdjustColumn2 = column2Adjust.css('paddingBottom'),
+        wasColumn1Adjusted = false,
+        wasColumn2Adjusted = false;
+
+    ok(column1.css('height') !== column2.css('height')); // Ensure heights are initially different.
+
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#with-initial-padding .column1',
+               "adjustSelector": '#with-initial-padding .column1 ul.items .last',
+               "adjustProperty": 'paddingBottom'
+           },
+           {
+               "measureSelector": '#with-initial-padding .column2',
+               "adjustSelector": '#with-initial-padding .column2 .title',
+               "adjustProperty": 'paddingBottom'
+           }
+        ]
+    });
+
+    ok(column1.css('height') === column2.css('height')); // Heights should now match.
+
+    wasColumn1Adjusted = column1Adjust.css('paddingBottom') !== initialPaddingAdjustColumn1;
+    wasColumn2Adjusted = column2Adjust.css('paddingBottom') !== initialPaddingAdjustColumn2;
+
+    ok((wasColumn1Adjusted && !wasColumn2Adjusted) || (!wasColumn1Adjusted && wasColumn2Adjusted)); // paddingBottom was either applied to column1's adjustSelector, or column2's adjustSelector: Not both.
+});
+
+test('Changing the adjust property changes the CSS property that is applied', function () {
+    var column1 = $('#border-test .column1'),
+        column2 = $('#border-test .column2'),
+        column1Adjust = column1.find('ul.items .last'),
+        column2Adjust = column2.find('.title'),
+        initialPaddingAdjustColumn1 = column1Adjust.css('paddingBottom'),
+        initialPaddingAdjustColumn2 = column2Adjust.css('paddingBottom'),
+        initialBorderAdjustColumn1 = column1Adjust.css('borderBottomWidth'),
+        initialBorderAdjustColumn2 = column2Adjust.css('borderBottomWidth'),
+        wasColumn1Adjusted = false,
+        wasColumn2Adjusted = false;
+
+    ok(column1.css('height') !== column2.css('height')); // Ensure heights are initially different.
+
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#border-test .column1',
+               "adjustSelector": '#border-test .column1 ul.items .last',
+               "adjustProperty": 'borderBottomWidth'
+           },
+           {
+               "measureSelector": '#border-test .column2',
+               "adjustSelector": '#border-test .column2 .title',
+               "adjustProperty": 'borderBottomWidth'
+           }
+        ]
+    });
+
+    ok(column1.css('height') === column2.css('height')); // Heights should now match.
+
+    // Padding should be unchanged.
+    ok(column1Adjust.css('paddingBottom') === initialPaddingAdjustColumn1);
+    ok(column2Adjust.css('paddingBottom') === initialPaddingAdjustColumn2);
+
+    wasColumn1Adjusted = column1Adjust.css('borderBottomWidth') !== initialBorderAdjustColumn1;
+    wasColumn2Adjusted = column2Adjust.css('borderBottomWidth') !== initialBorderAdjustColumn2;
+
+    ok((wasColumn1Adjusted && !wasColumn2Adjusted) || (!wasColumn1Adjusted && wasColumn2Adjusted)); // borderBottomWidth was either applied to column1's adjustSelector, or column2's adjustSelector: Not both.
+});
+
+module('Error handling');
+test('Error is not thrown if a measureSelector does not exist', function () {
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#main .column1',
+               "adjustSelector": '#main .column1 ul.items .last',
+               "adjustProperty": 'paddingBottom'
+           },
+           {
+               "measureSelector": '#element-does-not-exist .column2',
+               "adjustSelector": '#main .column2 .title',
+               "adjustProperty": 'paddingBottom'
+           }
+        ]
+    });
+
+    ok(true); // If we get here, it didn't cause an error.
+});
+
+test('Error is not thrown if an adjustSelector does not exist', function () {
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#main .column1',
+               "adjustSelector": '#element-does-not-exist .column1 ul.items .last',
+               "adjustProperty": 'paddingBottom'
+           },
+           {
+               "measureSelector": '#main .column2',
+               "adjustSelector": '#element-does-not-exist .column2 .title',
+               "adjustProperty": 'paddingBottom'
+           }
+        ]
+    });
+
+    ok(true); // If we get here, it didn't cause an error.
+});
+
+test('defaultAdjustProperty is used if not specified', function () {
+    var column1 = $('#default-adjust-property-test .column1'),
+        column2 = $('#default-adjust-property-test .column2'),
+        column1Adjust = column1.find('ul.items .last'),
+        column2Adjust = column2.find('.title'),
+        initialHeightAdjustColumn1 = column1Adjust.css('height'),
+        initialHeightAdjustColumn2 = column2Adjust.css('height'),
+        initialPaddingAdjustColumn1 = column1Adjust.css('paddingBottom'),
+        initialPaddingAdjustColumn2 = column2Adjust.css('paddingBottom'),
+        wasColumn1Adjusted = false,
+        wasColumn2Adjusted = false;
+
+    ok(column1.css('height') !== column2.css('height')); // Ensure heights are initially different.
+
+    $('#test-content').heightEqualiser({
+        "targets": [
+           {
+               "measureSelector": '#default-adjust-property-test .column1',
+               "adjustSelector": '#default-adjust-property-test .column1 ul.items .last'
+           },
+           {
+               "measureSelector": '#default-adjust-property-test .column2',
+               "adjustSelector": '#default-adjust-property-test .column2 .title'
+           }
+        ]
+    });
+
+    ok(column1.css('height') === column2.css('height')); // Heights should now match.
+
+    // Padding should be unchanged.
+    ok(column1Adjust.css('paddingBottom') === initialPaddingAdjustColumn1);
+    ok(column2Adjust.css('paddingBottom') === initialPaddingAdjustColumn2);
+
+    wasColumn1Adjusted = column1Adjust.css('height') !== initialHeightAdjustColumn1;
+    wasColumn2Adjusted = column2Adjust.css('height') !== initialHeightAdjustColumn2;
+
+    ok((wasColumn1Adjusted && !wasColumn2Adjusted) || (!wasColumn1Adjusted && wasColumn2Adjusted)); // Padding was either applied to column1's adjustSelector, or column2's adjustSelector: Not both.
+});
